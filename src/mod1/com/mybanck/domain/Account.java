fa1c159390aad21c1246ea0738275dbbe79ed928
Ruslan Pipan <rus.pipan@ukr.net>
2020-06-12 21:22:35 +0300

    Standard account.

---
+++ src/mod1/com/mybanck/domain/Account.java
@@ -0,0 +1,57 @@
+package mod1.com.mybanck.domain;
+
+public class Account {
+
+    protected double balance;
+
+    /**
+     * Conctructor that provides initial balance
+     * @param  balance a must be positive
+     * */
+    public Account(double balance) {
+        if (balance >= 0){
+            this.balance = balance;
+        }else {
+            this.balance = 0;
+        }
+    }
+    public Account(){
+        balance = 0;
+    }
+
+    /**
+     * Method to add money to account
+     * @param amt a positive amount if maney
+     * */
+    public boolean deposit(double amt){
+       if (amt > 0){
+           balance += amt;
+           return true;
+       }
+       return false;
+    }
+
+    /**
+     *  Method to withdraw money
+     * @param amt a positive amount of money
+     * */
+    public boolean withdraw(double amt){
+       if (amt <= balance){
+           balance -= amt;
+           return true;
+       }
+       return false;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "balance=" + balance +
+                '}';
+    }
+
+}
