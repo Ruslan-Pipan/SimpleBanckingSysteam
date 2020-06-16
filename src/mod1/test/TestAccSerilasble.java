52d767b18aba5a347964771416991f7220197f43
Ruslan Pipan <rus.pipan@ukr.net>
2020-06-16 20:49:24 +0300

    Tested serilizable and deserilizable.

--- src/mod1/test/TestAccSerilasble.java
+++ src/mod1/test/TestAccSerilasble.java
@@ -1,7 +1,24 @@
 package mod1.test;
 
-public class TestAccSerilasble {
-    public static void main(String[] args) {
+import mod1.com.mybanck.domain.BadVerification;
+import mod1.com.mybanck.domain.CheckingAccount;
+import mod1.com.mybanck.domain.Cunsumers;
+import mod1.com.mybanck.domain.SavingAccount;
+import mod1.serilaseble.CunsumerDeserilizable;
+import mod1.serilaseble.CunsumerSerilizable;
 
+import java.io.*;
+
+public class TestAccSerilasble {
+    public static void main(String[] args) throws BadVerification {
+        Cunsumers cunstomer = new Cunsumers.Cunsumer("Ruslan", "Pipan").setAdress("Krovinka").setNumber("(380)686 536 489").build();
+        SavingAccount saving = new SavingAccount(1000,7);
+        CheckingAccount checking = new CheckingAccount(500,100);
+        cunstomer.addAccount(saving);
+        cunstomer.addAccount(checking);
+
+        File file = new File("D:\\Навчання\\SimpleStsteamBanck\\src\\mod1\\test\\serilasbleTest.bin");
+        CunsumerSerilizable.serialesbleCunstomer(file,cunstomer);
+        System.out.println(CunsumerDeserilizable.deserilizableCunstomer(file));
     }
 }
