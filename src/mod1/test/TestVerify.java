ea171b6afe4d16c1440d1e3f258aad5a68c80a93
ruspi <rus.pipan@ukr.net>
2020-08-14 20:00:33 +0300

    Add new Verify for password.

---
+++ src/mod1/test/TestVerify.java
@@ -0,0 +1,9 @@
+package mod1.test;
+
+import mod1.com.mybanck.domain.Verification;
+
+public class TestVerify {
+    public static void main(String[] args) {
+        System.out.print(Verification.verifyPassword("Max-width:12"));
+    }
+}
