2ec17745fb672a24de6cbfb75593f58f80c49d67
ruspi <rus.pipan@ukr.net>
2020-08-15 19:50:41 +0300

    Reworked Connection in db bank.

--- src/db/ConnectionBank.java
+++ src/db/ConnectionBank.java
@@ -6,19 +6,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionBank {
-    private static Connection connection = null;
-    private static String URL = "jdbc:mysql://localhost:3306/bank?useUnicode=true&serverTimezone=UTC";
-    private static String PASSWORD = "Ruslan12619";
-    private static String USER_NAME = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/bank?useUnicode=true&serverTimezone=UTC";
+    private static final String PASSWORD = "Ruslan12619";
+    private static final String USER_NAME = "root";
 
-    protected static Connection conn() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        if (connection != null){
-            return connection;
-        }
-        else {
-            connection = DriverManager.getConnection(URL,USER_NAME, PASSWORD);
-            return connection;
-        }
+    public static Connection getConn() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL,USER_NAME,PASSWORD);
     }
 }
