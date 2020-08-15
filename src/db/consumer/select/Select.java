4e0413aa1314795c83824455fd231a95049d5218
ruspi <rus.pipan@ukr.net>
2020-08-15 19:59:37 +0300

    This abstract classed intended for software corrected contract with classes SelectXXX.

---
+++ src/db/consumer/select/Select.java
@@ -0,0 +1,43 @@
+package db.consumer.select;
+
+import com.sun.rowset.CachedRowSetImpl;
+import db.ConnectionBank;
+
+import javax.sql.RowSet;
+import javax.sql.rowset.CachedRowSet;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class Select {
+
+     abstract public void  select();
+
+     protected static void resultDefolt(String sql){
+          try (Connection connection = ConnectionBank.getConn();
+               Statement statement = connection.createStatement();
+               ResultSet resultSet = statement.executeQuery(sql)
+          ) {
+
+               resultSet.next();
+
+               CachedRowSet cachedRowSet = new CachedRowSetImpl();
+               cachedRowSet.populate(resultSet);
+
+               System.out.println("id | \t Name \t\t Last Name \t\t Email \t\t\t\t Phone Number \t\t Password \t\t\t Adress1 \n\n");
+               int id = resultSet.getInt(1);
+               String firstName = resultSet.getNString(2);
+               String lastName = resultSet.getNString(3);
+               String email = resultSet.getNString(4);
+               String phoneNumber = resultSet.getNString(5);
+               String password = resultSet.getNString(6);
+               String adress = resultSet.getNString(7);
+
+
+               System.out.println(id + "\t\t" + firstName + ",\t\t" + lastName + ",\t\t" + email + ",\t\t" + phoneNumber + ",\t\t" + password + ",\t\t" + adress );
+          } catch (SQLException | ClassNotFoundException throwables) {
+               throwables.printStackTrace();
+          }
+     }
+}
