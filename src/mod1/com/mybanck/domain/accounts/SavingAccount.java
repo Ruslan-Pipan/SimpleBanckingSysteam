78563421579d3d8bffdcc974736a1a6988da4b36
ruspi <rus.pipan@ukr.net>
2020-08-24 18:53:17 +0300

    Add nes field version.

--- src/mod1/com/mybanck/domain/accounts/SavingAccount.java
+++ src/mod1/com/mybanck/domain/accounts/SavingAccount.java
@@ -1,15 +1,19 @@
 package mod1.com.mybanck.domain.accounts;
 
+import db.account.GenerateBankAcc;
 import loger.GoodLog;
 
 import java.io.Serializable;
 
 public class SavingAccount extends Account implements Serializable {
+    private static final int version = 2;
+
     private double interestRate;
     private static final long serialVersionUID = 1L;
 
     public SavingAccount(double balance, double interestRate) {
         this.balance = balance;
+        this.bankAccount = GenerateBankAcc.generate();
         this.interestRate = interestRate;
         GoodLog.getInstance().log("Create SavingAccount.");
     }
