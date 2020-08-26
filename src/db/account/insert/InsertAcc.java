719709840006d120202826a6e8d9f0062eb40896
ruspi <rus.pipan@ukr.net>
2020-08-26 20:36:02 +0300

    Changed extends InsertDefault and implements Insert on just extends InsertDefault.

--- src/db/account/insert/InsertAcc.java
+++ src/db/account/insert/InsertAcc.java
@@ -1,10 +1,9 @@
 package db.account.insert;
 
-import db.Insert;
 import mod1.com.mybanck.domain.Consumer;
 
 
-public class InsertAcc extends InsertDefault implements Insert {
+public class InsertAcc extends InsertDefault {
 
     private static final String sql = "INSERT INTO accounts(idCunsumer, balance, bank_acc) VALUES(?,?,?)";
 
