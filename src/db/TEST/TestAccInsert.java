489569e0a68bd72cfe95b3fc1bdf107ca14f7a48
ruspi <rus.pipan@ukr.net>
2020-08-26 20:39:23 +0300

    This classes for testing Acc insert.

---
+++ src/db/TEST/TestAccInsert.java
@@ -0,0 +1,15 @@
+package db.TEST;
+
+import db.Insert;
+import db.account.insert.InsertSavingAcc;
+import mod1.com.mybanck.domain.Consumer;
+import mod1.com.mybanck.domain.bankException.BadVerification;
+
+public class TestAccInsert {
+    public static void main(String[] args) throws BadVerification {
+        Insert insert = new InsertSavingAcc();
+
+        Consumer consumer = new Consumer.CunsumerBild("TestSavingAcc","TestSavingAcc").setNumber("380686536489").setPass("TestSavingAcc12").setEmail("TestSavingAcc@mail.ru").build();
+
+    }
+}
