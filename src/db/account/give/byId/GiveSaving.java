fcc2da45d6f071429ae03022ce081498eba4d8a1
ruspi <rus.pipan@ukr.net>
2020-09-01 16:07:11 +0300

    This classes extents Give and Override method give() which returns Saving acc.

---
+++ src/db/account/give/byId/GiveSaving.java
@@ -0,0 +1,39 @@
+package db.account.give.byId;
+
+import db.ConnectionBank;
+import db.account.give.Give;
+import mod1.com.mybanck.domain.accounts.Account;
+import mod1.com.mybanck.domain.accounts.SavingAccount;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GiveSaving extends Give {
+
+    public GiveSaving(int id) {
+        this.id = id;
+        this.sql = SQLByID.SAVING.getSql() + id;
+    }
+
+    @Override
+    public SavingAccount give() {
+        try(Connection connection = ConnectionBank.getConn();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+        ) {
+            if (resultSet.next()){
+                Account account = giveDefaultAcc();
+                double interestRate  = resultSet.getDouble("interest_rate");
+                SavingAccount savingAccount = new SavingAccount(account);
+                savingAccount.setInterestRate(interestRate);
+                return savingAccount;
+            }
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+}
