37ccb4160bd0bcac5dca0e82c81befca596eda9a
ruspi <rus.pipan@ukr.net>
2020-09-01 16:02:50 +0300

    Add checking balance in contracture, and add new contracture which accepts Account.

--- src/mod1/com/mybanck/domain/accounts/CheckingAccount.java
+++ src/mod1/com/mybanck/domain/accounts/CheckingAccount.java
@@ -16,15 +16,30 @@ public class CheckingAccount extends Account implements Serializable {
     private static final long serialVersionUID = 1L;
 
     public CheckingAccount(double balance, double overdraftAmount) {
-        this.balance = balance;
+        if (balance >= 0)
+            this.balance = balance;
+        else
+            this.balance = 0;
+
+        if (overdraftAmount > 0)
+            this.overdraftAmount = overdraftAmount;
+        else
+            this.overdraftAmount = 0;
+
         this.bankAccount = GenerateBankAcc.generate();
-        this.overdraftAmount = overdraftAmount;
         GoodLog.getInstance().log("Create CheckingAccount.");
     }
 
     public CheckingAccount(double initBalance) {
         this(initBalance,0);
         GoodLog.getInstance().log("Create CheckingAccount with initBalance.");
+    }
+
+    public CheckingAccount(Account account){
+        this.balance = account.balance;
+        this.id = account.id;
+        this.idCunsumer = account.idCunsumer;
+        this.bankAccount = account.bankAccount;
     }
 
     @Override
@@ -36,5 +51,20 @@ public class CheckingAccount extends Account implements Serializable {
         }
         BadLog.getInstance().log("Error! Insufficient funds");
         throw new OverdraftExeption(amt - balance - overdraftAmount, "Error! Insufficient funds");
+    }
+
+    public void setOverdraftAmount(double overdraftAmount) {
+        this.overdraftAmount = overdraftAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "CheckingAccount{" +
+                ", id=" + id +
+                ", idCunsumer=" + idCunsumer +
+                ", bankAccount=" + bankAccount +
+                ", balance=" + balance +
+                ", overdraftAmount=" + overdraftAmount +
+                '}';
     }
 }
