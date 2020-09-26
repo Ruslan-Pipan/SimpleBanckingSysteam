95969082049e2123c727c514ab408584ea40e222
ruspi <rus.pipan@ukr.net>
2020-09-26 17:23:14 +0300

    Сorrected mistakes

---
+++ src/main/java/webapp/Table.java
@@ -0,0 +1,15 @@
+package webapp;
+
+
+public class Table {
+
+    private String text = "Test";
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
