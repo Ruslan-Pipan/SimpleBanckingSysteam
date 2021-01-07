89f8043c3c2a33f4927c32d6da0b422ee3ca079b
ruspi <rus.pipan@ukr.net>
2021-01-07 19:11:58 +0200

    This interface can only finding accounts, by different identifier.

---
+++ src/main/java/newDao/repositories/interfaces/AccountRepository.java
@@ -0,0 +1,14 @@
+package newDao.repositories.interfaces;
+
+import entety.Consumer;
+import entety.accounts.Account;
+
+import java.util.List;
+
+
+public interface AccountRepository<T extends Account>{
+    Account findAccountByBankAccount(long bankAcc);
+    List<T> findAccountsByConsumerId(int consumerId);
+    Account findAccountById(int id);
+    List<T> findAccountsConsumer(Consumer consumer);
+}
