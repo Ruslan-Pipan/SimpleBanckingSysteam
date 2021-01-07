0f14713b1ec3daa4ba51df14e35b1a518431a82b
ruspi <rus.pipan@ukr.net>
2021-01-07 19:17:51 +0200

    This interface can do manipulation with all accounts.

---
+++ src/main/java/newDao/services/interfaces/AccountService.java
@@ -0,0 +1,12 @@
+package newDao.services.interfaces;
+
+import bankException.DontInitialisation;
+import entety.accounts.Account;
+import newDao.repositories.interfaces.AccountRepository;
+
+public interface AccountService<T extends Account> extends AccountRepository<T>{
+    boolean removeAccount(T account);
+    boolean addAccount(T account) throws DontInitialisation;
+    boolean transaction(T accountFrom, T accountTo, double awt);
+    boolean updateBalance(T account);
+}
