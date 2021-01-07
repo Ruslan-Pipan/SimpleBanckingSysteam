27ac273e8957c6a404d4a93576aa0e7c2cd36f0d
ruspi <rus.pipan@ukr.net>
2021-01-07 19:35:21 +0200

    Is singleton for connection DB.

---
+++ src/main/java/dao/ConnectionBank.java
@@ -0,0 +1,20 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionBank {
+    private static final String URL = "jdbc:mysql://localhost:3306/bank?useSSL=false&useUnicode=true&serverTimezone=UTC";
+    private static final String PASSWORD = "Ruslan12619";
+    private static final String USER_NAME = "root";
+
+    public static synchronized Connection getConn() throws ClassNotFoundException, SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL,USER_NAME,PASSWORD);
+    }
+}
