770d72a2f3ee820b6f5cf9b55d7432e85574d6df
ruspi <rus.pipan@ukr.net>
2021-01-30 14:42:23 +0200

    This annotations for get request.

---
+++ src/main/java/service/dispatcher/annotations/Post.java
@@ -0,0 +1,13 @@
+package service.dispatcher.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Post {
+    String value();
+}
